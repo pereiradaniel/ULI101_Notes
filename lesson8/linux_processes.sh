7345c359fa679565a6e4f5555b27bec913f97a85
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-12-11 07:23:59 -0500

    Added lesson8

---
+++ lesson8/linux_processes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Display process status with PID for current terminal
+ps
+
+# Display additional information such ass PPID
+ps -l
+
+#Dsiplay status for all terminals belonging to username
+ps -U daniel
+
+# Display processes showing parent-child relatinships
+ps afux
+
+# Display resource usage
+top
+
+# Show job number of background processes
+jobs
+
